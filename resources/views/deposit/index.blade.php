b97840a000dda40b692361ee715bec59fd476bb0
faizirfan <faizirfan489@gmail.com>
2024-09-19 10:04:39 +0800

    feat(deposit) : index updated

--- resources/views/deposit/index.blade.php
+++ resources/views/deposit/index.blade.php
@@ -10,7 +10,8 @@
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Claims</h5>
-                    <a href="{{ route('claim.create') }}" class="btn btn-primary">New Claim</a>
+                    <a href="{{ route('claim.create') }}" class="btn btn-primary">New Deposit</a>
+                    <a href="{{ route('deposit.export') }}" class="btn btn-primary">Export</a>
 
                     <table id="tableData" class="datatable table">
                         <thead>
@@ -26,46 +27,18 @@
                             @php
                                 // dd($claims)
                             @endphp
-                            @foreach ($claims as $item)
+                            @foreach ($deposit as $item)
                                 <tr>
-                                    <td>{{ $item->claim_id }}</td>
+                                    <td>{{ $item->id }}</td>
                                     <td>
-                                        @if ($item->category == 'members')
-                                            <span class="badge bg-success">Members Rental</span>
-                                        @elseif($item->category == 'extra')
-                                            <span class="badge bg-secondary">Extra Job</span>
-                                        @elseif($item->category == 'depo')
-                                            <span class="badge bg-info">Morning Depo</span>
-                                        @elseif($item->category == 'claims')
-                                            <span class="badge bg-dark">Staff Claims</span>
-                                        @endif
+
                                     </td>
                                     <td>
-                                        @if ($item->status == 'approved')
-                                            <span class="badge bg-success">Approved</span>
-                                        @elseif($item->status == 'declined')
-                                            <span class="badge bg-danger">Declined</span>
-                                        @else
-                                            <span class="badge bg-warning">Pending</span>
-                                        @endif
+
                                     </td>
-                                    <td>{{ $item->payment_date }}</td>
+                                    <td></td>
                                     <td>
-                                        <div class="row">
-                                            <div class="col">
-                                                <a href="{{ route('claim.edit', $item->claim_id) }}"
-                                                    class="btn btn-primary btn-sm">Edit</a>
-                                            </div>
-                                            <div class="col">
-                                                <a href="{{ route('claim.show', ['id' => $item->claim_id, 'category' => $item->category]) }}"
-                                                    class="btn btn-primary btn-sm">Show</a>
-                                            </div>
-                                            <div class="col">
-                                                <form action="{{ route('claim.destroy', $item->claim_id) }}" method="post">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                                                </form>
+
                                     </td>
                                 </tr>
                             @endforeach
