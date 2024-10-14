9fd0aaae88e50a5923b57c52131c8b335882e1cc
faizirfan <faizirfan489@gmail.com>
2024-10-14 12:39:43 +0800

    feat(ui) : navbar

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -195,10 +195,9 @@
               <span>Claims</span>
               </a>
           </li><!-- End Profile Page Nav -->
-
           <li class="nav-item">
               <a class="nav-link collapsed" href="{{ route('fleet.index') }}">
-                  <i class="bi bi-question-circle"></i>
+                  <i class="bi bi-car-front"></i>
                   <span>Fleet</span>
               </a>
           </li><!-- End F.A.Q Page Nav -->
@@ -218,22 +217,25 @@
 
           <li class="nav-item">
               <a class="nav-link collapsed" href="{{ route('customer.index') }}">
-                  <i class="bi bi-card-list"></i>
+                  <i class="bi bi-people-fill"></i>
                   <span>Customer</span>
               </a>
           </li><!-- End Register Page Nav -->
-          <li class="nav-item">
+          {{-- <li class="nav-item">
               <a class="nav-link collapsed" href="{{ route('customer.index') }}">
                   <i class="bi bi-card-list"></i>
                   <span>Sales</span>
               </a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link collapsed" href="{{ route('user.index') }}">
-                  <i class="bi bi-box-arrow-in-right"></i>
-                  <span>User</span>
-              </a>
-          </li><!-- End Login Page Nav -->
+          </li> --}}
+          @can('delete user')
+              <li class="nav-item">
+                  <a class="nav-link collapsed" href="{{ route('user.index') }}">
+                      <i class="bi bi-person-fill"></i>
+                      <span>User</span>
+                  </a>
+              </li><!-- End Login Page Nav -->
+          @endcan
+
           {{--
       <li class="nav-item">
         <a class="nav-link collapsed" href="pages-error-404.html">
